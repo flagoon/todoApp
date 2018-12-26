b889d6d2a498991ef3bde4f4978e18d026dbeaee
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-12-27 00:09:46 +0100

    Changed TodoItem interface and array.

--- src/data/todo-list.ts
+++ src/data/todo-list.ts
@@ -1,29 +1,22 @@
 export interface ITodoItem {
-  todoID: number;
   name: string;
   isDone: boolean;
-  className?: string;
-  onClickHandler?: (todoID: number) => any;
 }
 
 export const todoList: ITodoItem[] = [
   {
-    todoID: 1,
     name: 'Walk the dog',
     isDone: false,
   },
   {
-    todoID: 2,
     name: 'Make todo app',
     isDone: false,
   },
   {
-    todoID: 3,
     name: 'Learn react',
     isDone: false,
   },
   {
-    todoID: 4,
     name: 'Be a nice guy',
     isDone: true,
   },
