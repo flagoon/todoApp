1891afd34f4bf734520e843dba2dc3381933cc9b
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-01-01 22:37:47 +0100

    Added styles for wrapper.

--- src/compoConto/App.tsx
+++ src/compoConto/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { todoList } from '../data/todo-list';
 import InputContainer from './Input/InputContainer';
 import { TodoList } from './Todos/TodoContainer';
+import styled from 'styled-components';
 
 class App extends React.Component {
   public state = {
@@ -24,12 +25,20 @@ class App extends React.Component {
 
   public render() {
     return (
-      <>
+      <MainAppArea>
         <InputContainer onSubmit={this.onSubmit} />
         <TodoList todoList={this.state.todoList} />
-      </>
+      </MainAppArea>
     );
   }
 }
+
+const MainAppArea = styled.div`
+  width: 540px;
+  height: 450px;
+  border: 1px solid green;
+  margin: 40px auto;
+  padding: 20px;
+`;
 
 export default App;
